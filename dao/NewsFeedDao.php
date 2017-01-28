cb43230dbddccca213413ccf93ed2173382e7a72
Andreas Tscheinig <andreas-tscheinig@gmx.at>
2017-01-28 23:19:40 +0100

    NewsFeed expiration date
    Items older than 1 week will not be displayed

--- dao/NewsFeedDao.php
+++ dao/NewsFeedDao.php
@@ -21,7 +21,7 @@ class NewsFeedDao extends AbstractDao{
 
     public function getByCommunity($community_oid) {
         $sql = NewsFeedDao::getBaseSql();
-        $sql->where("nf.community_oid = ?");
+        $sql->where("nf.community_oid = ? AND DATEDIFF(NOW(), nf.date_created) < 7");
         $sql->orderBy("nf.date_created desc");
         
         $records = Datenbarsch::getInstance()->fishQuery($sql, "s", $community_oid);
